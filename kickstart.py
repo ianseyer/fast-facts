95732dd791a9bcbf786743969dbc1e51ad420335
Ian Seyer <ian.seyer@gmail.com>
2014-11-08 21:39:26 -0600

    initial dump

--- kickstart.py
+++ kickstart.py
@@ -1,18 +1,32 @@
 """
 fast facts is an sms-based wikipedia query simplifier.
 
-query => translation thing => shortened first section
+query => translation thing => shortened first section of wikipedia
 
 """
 #foreign`
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 import requests, re
 from bs4 import BeautifulSoup
+import twilio.twiml
 
 #domestic
+#none
 
 app = Flask(__name__)
 app.secret_key = 'fastfacts'
+
+def handle_query(query):
+		request_url = "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+query+"&continue=&srprop=timestamp"
+		r = requests.get(request_url)
+
+		title = r.json()['query']['search'][0]['title']
+		request_url = "http://en.wikipedia.org/w/index.php?action=render&title="+title
+		response = requests.get(request_url)
+		soup = BeautifulSoup(response.text)
+		[each.decompose() for each in soup.find_all('table')]
+		out = soup.find_all('p')[0].get_text()[0:157]+"..."
+		return (out, title)
 
 @app.route('/search', methods=['GET', 'POST'])
 def query():
@@ -22,21 +36,20 @@ def query():
 		return render_template('search.html')
 
 	elif request.method == 'POST':
-		print 'method was post'
-		print request.form['q']
-		#display the form and results
-		query = request.form['q']
+		result = handle_query(request.form['q'])
+		return render_template('search.html', result=result[0], link="http://en.wikipedia.org/w/index.php?action=render&title="+result[1])
 
-		request_url = "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+query+"&continue=&srprop=timestamp"
-		r = requests.get(request_url)
+@app.route('/sms')
+def sms():
+	query = request.args['Body']
+	print query
+	result = handle_query(query)
 
-		title = r.json()['query']['search'][0]['title']
-		request_url = "http://en.wikipedia.org/w/index.php?action=render&title="+title
-		response = requests.get(request_url)
-		soup = BeautifulSoup(response.text)
-
-		out = soup.p.get_text()[0:157]+"..."
-		return render_template('search.html', result=out)
+	resp = twilio.twiml.Response()
+	print removeNonAscii(result[0])
+	resp.message(unicode(result[0]))
+	return str(resp)
+	
 
 if __name__ == '__main__':
 	app.run(debug=True)
